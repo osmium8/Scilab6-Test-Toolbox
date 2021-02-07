d0da3a58bb4978ac65950f053823ca9de2a0de7e
osmium8 <pranshudee8@gmail.com>
2021-02-07 21:15:56 +0530

    Update main.c

--- test/main.c
+++ test/main.c
@@ -3,13 +3,13 @@
 
 int main(void)
 {
+	printf("Inside main \n\n");
+	
 	int array[2][3] = {{1, 1, 1},
-                       {2, 2, 2}};
+                           {2, 2, 2}};
     
     	int transpose_array[3][2];
 	int n = 2, m = 3;
-	
-	printf("Inside main \n\n");
 	
 	printf("Input matrix \n");
 	int i, j;
@@ -21,9 +21,9 @@ int main(void)
         }
         
         printf("\ncalling transpose() ...\n\n");
-	printf("Output matrix \n");
         transpose(n, m, array, transpose_array);
 	
+	printf("Output matrix \n");
         for (i = 0; i < m; i++) {
       	    for (j = 0; j < n; j++)  {
         	printf("%d ", transpose_array[i][j]);
