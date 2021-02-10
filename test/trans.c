7cc2b07f7c884091fcb7608ecdcce47dd5ac325e
osmium8 <pranshudee8@gmail.com>
2021-02-10 15:18:42 +0530

    Update trans.c

--- test/trans.c
+++ test/trans.c
@@ -1,10 +1,10 @@
 /*
 * This c program returns transpose of a given matrix
 * paramters
-* n = number of rows
-* m = number of columns
-* A = input matrix (2D array, n*m)
-* B = output matrix (2D array, m*n) transpose of matrix A
+* rows = number of rows
+* columns = number of columns
+* matrix = input matrix (2D array, rows * columns)
+* t_matrix = output matrix (2D array, columns * rows), transpose of matrix
 */
 
 #include<stdio.h>
@@ -17,11 +17,8 @@ void trans(int rows, int columns, double **matrix, double **t_matrix)
     for (i = 0; i < columns; i++) {
 		for (j = 0; j < rows; j++)  {
 			t_matrix[i][j] = matrix[j][i];
-			//printf("%f ",t_matrix[i][j]);
-			//printf("%f ",matrix[j][i]);
 		}
     }
               
     return;
 } 
-
